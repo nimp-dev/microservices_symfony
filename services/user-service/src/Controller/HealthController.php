31b05e5e840704c70387c6783b3048c5b298a389
nimp-dev <egorkonopka93@gmail.com>
2025-10-11 19:26:22 +0300

    add health controllers

---
+++ services/user-service/src/Controller/HealthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HealthController extends AbstractController
+{
+    #[Route('/health', name: 'health_check')]
+    public function health(): JsonResponse
+    {
+        return $this->json([
+            'status' => 'ok',
+            'service' => 'order',
+            'timestamp' => time()
+        ]);
+    }
+}
